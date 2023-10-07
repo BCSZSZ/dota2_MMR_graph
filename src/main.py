673b3ae228eaf8611c56645391f9dfc83cb4dc72
BC_SZSZ <liaowang586@hotmail.com>
2023-10-07 11:57:25 +0900

    add function calculate_mmr_history_roughly output a point set.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import requests
 import json
-import os
 from pathlib import Path
+from collections import namedtuple
+import time
+
 
 def get_accountID_path(filename="accountID.json"):
     """
@@ -24,6 +26,7 @@ def get_player_match_path(playername="maofeng"):
     Returns:
     ./data/maofeng/matchdata.json
     """ 
+    # make sure sub dir exist.
     sub_dir=get_data_directory() / playername
     sub_dir.mkdir(parents=True, exist_ok=True)
 
@@ -48,7 +51,7 @@ def get_100_rank_match_data_and_save(playerName):
     Parameters:
     playername -- str,player's name like maofeng
     Returns:
-    match data json
+    matches -- matches data in json
     """
 
     json_path=get_accountID_path()
@@ -72,14 +75,55 @@ def get_100_rank_match_data_and_save(playerName):
     player_match_path=get_player_match_path(playerName)
     with open(player_match_path, "w") as json_file:
         json.dump(matches, json_file, indent=4)
+    print("matches")
+    print(matches)
     print(f"Data saved to {json_path} successfully!")
     return matches
 
+def calculate_mmr_history_roughly(matches=None,current_mmr=4670):
+    """    
+    calculate win lose info from the raw matches json.
+    It is a rough calculation, so win and lose is -+25.
+    return the mmr.
+    Parameters:
+    matches100 -- dict,player's matches data request from OpendotaAPI
+    Returns:
+    mmrList -- list of last 100 mmr.
+    """
+    
+    # test data
+    player_match_path=get_player_match_path("maofeng")
+    with open(player_match_path, "r") as json_file:
+        testMatches = json.load(json_file)
+        
+    Coordinate = namedtuple("Coordinate", ["timestamp", "mmr"])
+    current_timestamp_int = int(time.time())
+    start_point = Coordinate(current_timestamp_int, current_mmr)  # 
+
+    # 如果有多个坐标
+    points = []
+    points.append(start_point)
+        
+    for match in testMatches:
+        if  (match["player_slot"] <=127 and match["radiant_win"] == True) or (match["player_slot"] >=128 and match["radiant_win"] == False):
+            current_mmr=current_mmr-25
+        else:
+            current_mmr=current_mmr+25
+        points.append(Coordinate(match["start_time"],current_mmr))
+    print("points")
+    print(points)
+    return points
+    #
+    
         
 def main():
+    # who is playing?
     player="maofeng"
+    current_mmr=4670
     match_data=get_100_rank_match_data_and_save(player)
-    print(match_data)
+    mmr_Points=calculate_mmr_history_roughly(match_data,current_mmr)
+    mmr_Points.reverse()
+    print(mmr_Points)
 
 
 if __name__ == "__main__":
