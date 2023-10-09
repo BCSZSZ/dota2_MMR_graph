f4698dc6ac8ee1a866ae615ff6e968bd7a53cb35
BC_SZSZ <liaowang586@hotmail.com>
2023-10-09 19:13:24 +0900

    compelte the simple win rate calculate function.

--- src/main.py
+++ src/main.py
@@ -26,7 +26,6 @@ def generate_noise(num_points=100, noise_range=(-5, 5)):
     
     return noise
 
-
 def get_accountID_path(filename="accountID.json"):
     """
     Get the path to the accountID JSON file in the data directory.
@@ -73,7 +72,7 @@ def get_100_rank_match_data_and_save(playerName):
     Parameters:
     playername -- str,player's name like maofeng
     Returns:
-    matches -- matches data in json
+    matches -- matches data in json]
     """
 
     json_path=get_accountID_path()
@@ -94,11 +93,13 @@ def get_100_rank_match_data_and_save(playerName):
         return None
     # happy path
     matches = response.json()
+    matches.reverse()
+    match_count=len(matches)
     player_match_path=get_player_match_path(playerName)
     with open(player_match_path, "w") as json_file:
         json.dump(matches, json_file, indent=4)
     print("matches")
-    print(matches)
+    print(f"{match_count} matches are read")
     print(f"Data saved to {json_path} successfully!")
     return matches
 
@@ -130,8 +131,10 @@ def calculate_mmr_history_roughly(matches=None,current_mmr=4670):
         points.append(Coordinate(matches[i]["start_time"],current_mmr))
     print("points")
     print(points)
+    points.reverse()
     return points
     #
+    
 def plot_mmr_over_time_and_save(coordinates,player_name):
     # Extract timestamps and MMRs from the named tuples
     timestamps = [point.timestamp for point in coordinates]
@@ -163,16 +166,111 @@ def plot_mmr_over_time_and_save(coordinates,player_name):
     plt.savefig(filename,dpi=1080)
     plt.show()
         
+def calculate_win_rate_and_others_and_save(playerName,matches=None):
+    """calculate or collect some figure based on the match data
+    for now I think the following should be noted.
+    1 solo rank count
+    2 party rank count
+    3 solo rank win rate
+    4 party rank win rate
+    5 first match date
+    
+    the following should be interesting but not this time
+    all unique heroes played
+    most played hero and count
+    top win rate hero
+    
+    
+
+    Args:
+        matches (list, optional): recent 100 rank match data. Defaults to None.
+    """
+    win_rate=0
+    solo_rank_win_rate=0
+    party_rank_win_rate=0
+    unknown_rank_win_rate=0
+    first_match_date=0
+    solo_rank_count=0
+    party_rank_count=0
+    unknown_rank_count=0
+    
+    solo_win_count=0
+    solo_lose_count=0
+    party_win_count=0
+    party_lose_count=0
+    unknown_win_count=0
+    unknown_lose_count=0
+    match_count=len(matches)
+    for i in range(len(matches)):
+        if  (matches[i]["player_slot"] <=127 and matches[i]["radiant_win"] == True) or (matches[i]["player_slot"] >=128 and matches[i]["radiant_win"] == False):
+            if matches[i]["party_size"] ==None:
+                unknown_rank_count=unknown_rank_count+1
+                unknown_win_count=unknown_win_count+1
+            if matches[i]["party_size"] ==1:
+                solo_rank_count=solo_rank_count+1
+                solo_win_count=solo_win_count+1
+            if matches[i]["party_size"]  in (2,3,4,5):
+                party_rank_count=party_rank_count+1
+                party_win_count=party_win_count+1
+        else:
+            if matches[i]["party_size"] ==None:
+                unknown_rank_count=unknown_rank_count+1
+                unknown_lose_count=unknown_lose_count+1
+            if matches[i]["party_size"] ==1:
+                solo_rank_count=solo_rank_count+1
+                solo_lose_count=solo_lose_count+1
+            if matches[i]["party_size"] in (2,3,4,5):
+                party_rank_count=party_rank_count+1
+                party_lose_count=party_lose_count+1
+        if i == 1:
+            first_match_date=matches[i]["start_time"]
+            
+    solo_rank_win_rate=solo_win_count/solo_rank_count*100
+    party_rank_win_rate=party_win_count/party_rank_count*100
+    unknown_rank_win_rate=unknown_win_count/unknown_rank_count*100
+    win_rate=(solo_win_count+party_win_count+unknown_win_count)/(solo_rank_count+party_rank_count+unknown_rank_count)*100
+    
+    dt_object = datetime.utcfromtimestamp(first_match_date)
+    formatted_date = dt_object.strftime('%Y/%m/%d %H:%M:%S') 
+    
+    print(f"当前政审的是{playerName}")
+    print(f"本次读取了{match_count}条天梯比赛数据。")
+    print(f"其中，最早的一把是{formatted_date}打的")
+    print(f"{playerName}的总体胜率是 {win_rate}%")
+    print(f"{playerName}的单排把数是 {solo_rank_count}把")
+    print(f"{playerName}的单排胜率是 {solo_rank_win_rate}%")
+    print(f"{playerName}的组排把数是 {party_rank_count}把")
+    print(f"{playerName}的组排胜率是 {party_rank_win_rate}%")
+    print(f"{playerName}的组队状态不明天梯比赛把数是 {unknown_rank_count}把")
+    print(f"{playerName}的组队状态不明天体比赛胜率是 {unknown_rank_win_rate}%")
+    
+    
+    
+
 def main():
     # who is playing?
     player="liaoweiran"
     current_mmr=4670
+    #
+    
+    # get and save match data
     match_data=get_100_rank_match_data_and_save(player)
-    mmr_Points=calculate_mmr_history_roughly(match_data,current_mmr)
-    mmr_Points.reverse()
-    print(mmr_Points)
-    plot_mmr_over_time_and_save(mmr_Points,player)
-
+    #
+    
+    # calculate some stats based on the match data.
+    
+    calculate_win_rate_and_others_and_save(player,match_data)
+    
+    
+    
+    # # calculate rough mmr history and plot it.
+    # mmr_Points=calculate_mmr_history_roughly(match_data,current_mmr)
+    # plot_mmr_over_time_and_save(mmr_Points,player)
+    # #
+    
+    
+    
+    
 
 if __name__ == "__main__":
     main()
